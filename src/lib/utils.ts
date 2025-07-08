5f0d7ea0a15fe59747759be5de694f9496fc5871
SolwDawg <sondang24.sh@gmail.com>
2025-07-08 13:46:33 +0700

    confirm receivedSuccessfully

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -134,5 +134,13 @@ export const formatDate = {
     } else {
       return formatDate.dateOnly(dateString);
     }
+  },
+
+  deadline: (dateString: string) => {
+    return new Date(dateString).toLocaleDateString('vi-VN', {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+    });
   }
 };
